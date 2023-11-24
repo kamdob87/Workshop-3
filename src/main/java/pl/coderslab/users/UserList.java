44e6ed161a068d41746f8933733383ae676ca30f
kamil <kamdob87@gmail.com>
2023-11-24 14:01:07 +0100

    add display all users

--- src/main/java/pl/coderslab/users/UserList.java
+++ src/main/java/pl/coderslab/users/UserList.java
@@ -1,5 +1,8 @@
 package pl.coderslab.users;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -12,6 +15,16 @@ public class UserList extends HttpServlet {
 
         getServletContext().getRequestDispatcher("/users/list.jsp")
                 .forward(request, response);
+
+        User user = new User();
+        user.setUserName("Andrzej");
+        user.setId(1);
+        user.setEmail("andrew@gmail.com");
+
+        UserDao userDao = new UserDao();
+        userDao.create(user);
+        request.setAttribute("users", userDao.findAll());
+
     }
 
 }
